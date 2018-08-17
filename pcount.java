8e0326d8b03a6264e8af8ee5f49b871600885d63
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-17 12:18:14 +0530

    Create pcount.java

---
+++ pcount.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,c=0;
+	    Scanner s=new Scanner(System.in);
+	    String s1=s.nextLine();
+	    char ch;
+	    int n=s1.length();
+	    for(i=0;i<n;i++)
+	    {
+	    	ch=s1.charAt(i);
+	    	if(ch=='.')
+	    	{
+	    		c=c+1;
+	    	}
+	    }
+	    System.out.print(c+1);
+	    
+	}
+}
